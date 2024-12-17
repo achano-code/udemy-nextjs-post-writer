096124f315c30350e07919ff7e49be6c7b718f19
achano <hyuga.workplace@gmail.com>
2024-12-18 08:20:37 +0900

    トップページのxとgithubについてconfig追加

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "./config/site";
 export default function IndexPage() {
   return (
     <>
       <section className="pt-6 md:pt-10 lg:py-32 md:pb-12">
         <div className="container mx-auto text-center flex flex-col items-center gap-4">
           <Link
-            href={"/"}
+            href={siteConfig.links.x}
             className="bg-muted px-4 py-1.5rem rounded-2xl font-medium text-sm"
+            target="_blank"
+            rel="noreferrer"
           >
             xをフォローする
           </Link>
@@ -27,7 +30,7 @@ export default function IndexPage() {
               はじめる
             </Link>
             <Link
-              href={"/"}
+              href={siteConfig.links.github}
               className={cn(buttonVariants({ size: "lg", variant: "outline" }))}
               target="_blank"
               rel="noreferrer"
